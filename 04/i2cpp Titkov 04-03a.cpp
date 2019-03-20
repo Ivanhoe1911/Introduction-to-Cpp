5fc2261fc06774add143f52378cb21b3d1836a2b
Ivanhoe1911 <45850490+Ivanhoe1911@users.noreply.github.com>
2019-03-20 23:38:11 +0300

    Add files via upload

---
+++ 04/i2cpp Titkov 04-03a.cpp
@@ -0,0 +1,51 @@
+﻿/**
+*
+* Титков Иван
+* 3. Определить время года по номеру месяца двумя способами:
+* 1) использовать оператор if;
+*
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int month;		// Переменная для хранения номера месяца.
+
+	cout << "Введите номер месяца: ";	// Ввод месяца с клавиатуры.
+	cin >> month;
+	cout << "Вы ввели номер " << month << endl;
+
+	if (month == 1) // Конструкция if-else для перебора значений переменной 'month'.
+		cout << "Январь" << endl;
+	else if (month == 2)
+		cout << "Февраль" << endl;
+	else if (month == 3)
+		cout << "Март" << endl;
+	else if (month == 4)
+		cout << "Апрель" << endl;
+	else if (month == 5)
+		cout << "Май" << endl;
+	else if (month == 6)
+		cout << "Июнь" << endl;
+	else if (month == 7)
+		cout << "Июль" << endl;
+	else if (month == 8)
+		cout << "Август" << endl;
+	else if (month == 9)
+		cout << "Сентябрь" << endl;
+	else if (month == 10)
+		cout << "Октябрь" << endl;
+	else if (month == 11)
+		cout << "Ноябрь" << endl;
+	else if (month == 12)
+		cout << "Декабрь" << endl;
+	else
+		cout << "Нет такого месяца." << endl;
+
+	system("pause");
+}
---
+++ 04/i2cpp Titkov 04-03b.cpp
@@ -0,0 +1,67 @@
+﻿/**
+*
+* Титков Иван
+* 3. Определить время года по номеру месяца двумя способами:
+* 2) использовать оператор switch
+*
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int month;		// Переменная для хранения номера месяца.
+
+	cout << "Введите номер месяца: ";	// Ввод месяца с клавиатуры.
+	cin >> month;
+	cout << "Вы ввели номер " << month << endl;
+
+	switch (month)	// Конструкция switch-case для перебора значений переменной 'month'.
+	{
+	case 1:
+		cout << "Январь" << endl;
+		break;
+	case 2:
+		cout << "Февраль" << endl;
+		break;
+	case 3:
+		cout << "Март" << endl;
+		break;
+	case 4:
+		cout << "Апрель" << endl;
+		break;
+	case 5:
+		cout << "Май" << endl;
+		break;
+	case 6:
+		cout << "Июнь" << endl;
+		break;
+	case 7:
+		cout << "Июль" << endl;
+		break;
+	case 8:
+		cout << "Август" << endl;
+		break;
+	case 9:
+		cout << "Сентябрь" << endl;
+		break;
+	case 10:
+		cout << "Октябрь" << endl;
+		break;
+	case 11:
+		cout << "Ноябрь" << endl;
+		break;
+	case 12:
+		cout << "Декабрь" << endl;
+		break;
+	default:
+		cout << "Нет такого месяца" << endl;
+		break;
+	}
+
+	system("pause");
+}
